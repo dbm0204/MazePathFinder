828b6dd76210b8a71f0b7b80d62870ae7adf61ef
Ben Mathew <dbm0204@gmail.com>
2018-08-20 15:00:38 -0400

    Random Maze Generator Algorithm Implemented

---
+++ MazeGenerator.py
@@ -0,0 +1,59 @@
+import numpy
+from numpy.random import random_integers as rand
+import matplotlib.pyplot as pyplot
+from errno import EEXIST
+from os import makedirs, path
+
+def maze(width=50, height=50, complexity=0.05, density=0.75):
+    # Only odd shapes
+    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
+    # Adjust complexity and density relative to maze size
+    complexity = int(complexity * (5 * (shape[0] + shape[1]))) # number of components
+    density    = int(density * ((shape[0] // 2) * (shape[1] // 2))) # size of components
+    # Build actual maze
+    Z = numpy.zeros(shape, dtype=bool)
+    # Fill borders
+    Z[0, :] = Z[-1, :] = 1
+    Z[:, 0] = Z[:, -1] = 1
+    # Make aisles
+    for i in range(density):
+        x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2 # pick a random position
+        Z[y, x] = 1
+        for j in range(complexity):
+            neighbours = []
+            if x > 1:             neighbours.append((y, x - 2))
+            if x < shape[1] - 2:  neighbours.append((y, x + 2))
+            if y > 1:             neighbours.append((y - 2, x))
+            if y < shape[0] - 2:  neighbours.append((y + 2, x))
+            if len(neighbours):
+                y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
+                if Z[y_, x_] == 0:
+                    Z[y_, x_] = 1
+                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                    x, y = x_, y_
+    return Z
+
+def mkdir_p(mypath):
+    '''Creates a directory. equivalent to using mkdir -p on the command line'''
+    try:
+        makedirs(mypath)
+    except OSError as exc: # Python >2.5
+        if exc.errno == EEXIST and path.isdir(mypath):
+            pass
+        else: raise
+
+
+def generate_data(size=4):
+    for i in range(0,size):
+        pyplot.figure(figsize=(10, 5))
+        pyplot.imshow(maze(80, 40), cmap=pyplot.cm.binary, interpolation='nearest')
+        pyplot.xticks([]), pyplot.yticks([])
+        pyplot.show()
+        #pyplot.savefig(str(i)+'.png')
+
+
+def main():
+    generate_data(2)
+
+if __name__=='__main__':
+    main()
